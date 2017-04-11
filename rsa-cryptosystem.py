f983c1f82e29eec10218140b1d923127455d52c8
Flex2424 <fakestratocaster@yandex.ru>
2017-04-12 01:38:00 +0300

    decrypt triple-des

--- rsa-cryptosystem.py
+++ rsa-cryptosystem.py
@@ -1,30 +1,49 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
+from __future__ import print_function
 from os import urandom
 from Crypto.Cipher import DES3
 import constants
 
 
 class RSACryptoSystem:
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, in_file, out_file):
+        self.in_file = in_file
+        self.out_file = out_file
         self.cipher_text = None
+        self.key = urandom(24)
 
     def encrypt_triple_des(self):
-        des = DES3.new(urandom(24), DES3.MODE_ECB)
+        des = DES3.new(self.key, DES3.MODE_ECB)
         try:
-            with open(self.filename, 'r') as file:
-                while True:
-                    block = file.read(DES3.block_size)
-                    if len(block) == 0:
-                        break
-                    elif len(block) != DES3.block_size:
-                        block += ' ' * (DES3.block_size - len(block))
-                    print(des.encrypt(block))
+            with open(self.in_file, 'r') as file:
+                with open(self.out_file, 'w') as out:
+                    while True:
+                        block = file.read(DES3.block_size)
+                        if len(block) == 0:
+                            break
+                        elif len(block) != DES3.block_size:
+                            block += ' ' * (DES3.block_size - len(block))
+                        out.write(des.encrypt(block))
         except:
-            print("Can't fine file!")
+            print('Encrypt error!')
             exit(-1)
-        print('[+] 3DES - Done!')
+        print('[+] 3DES - Encrypt Done!')
+
+    def decrypt_triple_des(self):
+        des = DES3.new(self.key, DES3.MODE_ECB)
+        try:
+            with open(self.out_file, 'r') as file:
+                with open('decrypted.txt', 'w') as out:
+                    while True:
+                        block = file.read(DES3.block_size)
+                        if len(block) == 0:
+                            break
+                        out.write(des.decrypt(block))
+        except:
+            print('Decrypt error!')
+            exit(-1)
+        print('[+] 3DES - Decrypt Done!')
 
     def rsa_encrypt(self):
         pass
@@ -33,6 +52,6 @@ class RSACryptoSystem:
         pass
 
 
-rsa = RSACryptoSystem('data_to_encrypt.txt')
+rsa = RSACryptoSystem('data_to_encrypt.txt', 'cipher_text.txt')
 rsa.encrypt_triple_des()
-
+rsa.decrypt_triple_des()

120a5bb83df98ae662670c48ae8aaea9f8148ed1
Flex2424 <fakestratocaster@yandex.ru>
2017-04-12 01:08:42 +0300

    Triple-DES

--- rsa-cryptosystem.py
+++ rsa-cryptosystem.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
 
 from os import urandom
-import Crypto
+from Crypto.Cipher import DES3
 import constants
 
 
 class RSACryptoSystem:
     def __init__(self, filename):
-        # random key for 3DES
-        self.key = urandom(24)
-
-        with open(filename, 'r') as file:
-            self.plain_text = file.read()
+        self.filename = filename
+        self.cipher_text = None
 
     def encrypt_triple_des(self):
-        pass
+        des = DES3.new(urandom(24), DES3.MODE_ECB)
+        try:
+            with open(self.filename, 'r') as file:
+                while True:
+                    block = file.read(DES3.block_size)
+                    if len(block) == 0:
+                        break
+                    elif len(block) != DES3.block_size:
+                        block += ' ' * (DES3.block_size - len(block))
+                    print(des.encrypt(block))
+        except:
+            print("Can't fine file!")
+            exit(-1)
+        print('[+] 3DES - Done!')
 
     def rsa_encrypt(self):
         pass
@@ -24,4 +34,5 @@ class RSACryptoSystem:
 
 
 rsa = RSACryptoSystem('data_to_encrypt.txt')
+rsa.encrypt_triple_des()
 

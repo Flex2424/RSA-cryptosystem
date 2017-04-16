aa5c1fe4c2394e279048c273d099556b6ee3eb29
Flex2424 <fakestratocaster@yandex.ru>
2017-04-16 19:06:16 +0300

    encryption -> asn1-file

--- parser.py
+++ parser.py
@@ -12,7 +12,7 @@ class ASN1:
         asn.enter(asn1.Numbers.Set)
         asn.enter(asn1.Numbers.Sequence)
         asn.write(b'\x00\x06', asn1.Numbers.OctetString)
-        asn.write(b'RSA11. DimaZyuzin', asn1.Numbers.UTF8String)
+        asn.write(b'RSASignature. DimaZyuzin', asn1.Numbers.UTF8String)
         asn.enter(asn1.Numbers.Sequence)
         asn.write(modulus, asn1.Numbers.Integer)
         asn.write(exp, asn1.Numbers.Integer)
@@ -29,8 +29,32 @@ class ASN1:
         asn.leave()
         return asn.output()
 
-    def encode_file(self):
-        pass
+    @staticmethod
+    def encode_file(modulus, exp, cipher_text, length):
+        file = asn1.Encoder()
+        file.start()
+        file.enter(asn1.Numbers.Sequence)
+        file.enter(asn1.Numbers.Set)
+        file.enter(asn1.Numbers.Sequence)
+        file.write(b'\x00\x01', asn1.Numbers.OctetString)
+        file.write(b'Encryption. DimaZyuzin', asn1.Numbers.UTF8String)
+        file.enter(asn1.Numbers.Sequence)
+        file.write(modulus, asn1.Numbers.Integer)
+        file.write(exp, asn1.Numbers.Integer)
+        file.leave()
+        file.enter(asn1.Numbers.Sequence)
+        file.leave()
+        file.enter(asn1.Numbers.Sequence)
+        file.write(cipher_text, asn1.Numbers.Integer)
+        file.leave()
+        file.leave()
+        file.leave()
+        file.enter(asn1.Numbers.Sequence)
+        file.write(b'\x01\x32', asn1.Numbers.OctetString)
+        file.write(length, asn1.Numbers.Integer)
+        file.leave()
+        file.leave()
+        return file.output()
 
     def parse_file(self):
         pass
